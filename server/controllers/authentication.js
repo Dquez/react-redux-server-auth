8d99a240d9ad48b27441cf1eba566b25abc70b7c
Dariell Vasquez <dariellv@yahoo.com>
2018-12-04 12:02:30 -0500

    bcrypt installed to hash password, JWT to send token back from signup request

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,23 +1,35 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    // user.id === user._id
+    return jwt.encode({sub:user.id, iat: timestamp}, config.secret);
+}
 
 exports.signup = function(req,res,next){
-    // See if a user with given email exists
-    console.log(req.body)
+    
     const email = req.body.email;
     const password = req.body.password;
+    if(!email || !password){
+        res.status(422).send({error: 'You must provide a password and email'})
+    }
+    // See if a user with given email exists
     User.findOne({email: email}, function(err, existingUser){
-        if(!existingUser){
-            User.create(req.body, function(err, userCB){
-                if(err) throw err;
-                console.log(userCB)
-            } );
+        if(err) {return next(err);}
+
+        // if user with email does exist, return error
+        if(existingUser){
+            return res.status(422).send({error: 'Email is in use'})
         }
+        // if user w/ email does not exist, create and save user
+        const user = new User(req.body);
+        user.save(function(err){
+            if(err) {return next(err);}
+            // respond to request indicating the user was create
+            res.send({token: tokenForUser(user)})
+        })
     })
-
-    // if user with email does exist, return error
-
-    // if user w/ email does not exist, create and save user
-
-    // respond to request indicating the user was create
-    res.send({success:true})
 }
